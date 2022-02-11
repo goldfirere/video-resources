f05c971be3c877e50fa6aa2912c709732338addf
Richard Eisenberg <rae@richarde.dev>
2022-02-10 23:52:44 -0500

    Generating responses and advancing the state

--- 2022-wordle/Wordle.hs
+++ 2022-wordle/Wordle.hs
@@ -5,7 +5,55 @@ import qualified Data.Map.Strict as M
 import qualified Data.Vector as V
 import qualified Data.Text as T
 
+-- KNOWN BUG: Does not work for words with repeated letters
+
 import Words
+
+-------------------------
+-- utility
+
+lettersInWord :: WordleWord -> S.Set Char
+lettersInWord word = V.foldl' (flip S.insert) S.empty (getLetters word)
+
+allLocations :: [Location]
+allLocations = [0 .. 4]
+
+-------------------------
+-- responses to guesses
+
+data LetterResponse = Gray | Yellow | Green
+
+instance Show LetterResponse where
+  show Gray = "X"
+  show Yellow = "Y"
+  show Green = "G"
+
+newtype Response = MkResponse (Vec5 LetterResponse)
+
+getResponses :: Response -> Vec5 LetterResponse
+getResponses (MkResponse rs) = rs
+
+instance Show Response where
+  show (MkResponse responses) = concatMap show (V.toList responses)
+
+respondToGuess :: WordleWord   -- guess
+               -> WordleWord   -- answer
+               -> Response     -- pattern of gray, yellow, and green boxes
+respondToGuess guess answer = MkResponse (V.zipWith convert green_spots (getLetters guess))
+  where
+    answer_letters = lettersInWord answer
+
+    green_spots :: Vec5 Bool
+    green_spots = V.zipWith (==) (getLetters guess) (getLetters answer)
+
+    convert :: Bool -> Char -> LetterResponse
+    convert False the_letter | the_letter `S.member` answer_letters = Yellow
+                             | otherwise                            = Gray
+    convert True  _ = Green
+
+
+-------------------------
+-- Game state
 
 -- for each letter in the alphabet, either:
 --   1) we know nothing: not in the State map
@@ -17,12 +65,64 @@ type Location = Int   -- Int in the range 0 - 4
 data LetterInformation
   = LetterNotInWord
   | LetterNotInLocations (S.Set Location)  -- set of places letter can *not* appear
+  deriving Show
 
 data State = MkState (M.Map Char LetterInformation)
 
-filterWords :: State -> V.Vector WordleWord -> V.Vector WordleWord
-filterWords (MkState mapping) words = V.filter (\w -> no_letters_in_bad_locations w && all_required_letters w) words
+instance Show State where
+  show (MkState mapping) = show mapping
+
+startingState :: State
+startingState = MkState mempty
+
+instance Semigroup State where
+  (MkState map1) <> (MkState map2) = MkState (M.unionWith combine map1 map2)
+    where
+      combine :: LetterInformation -> LetterInformation -> LetterInformation
+      combine LetterNotInWord LetterNotInWord = LetterNotInWord
+      combine LetterNotInWord (LetterNotInLocations {}) = error "duplicate letter (1)"
+      combine (LetterNotInLocations {}) LetterNotInWord = error "duplicate letter (2)"
+      combine (LetterNotInLocations locs1) (LetterNotInLocations locs2) = LetterNotInLocations (locs1 <> locs2)
+
+instance Monoid State where
+  mempty = startingState
+
+-------------------------------------
+-- advancing state
+
+advanceState :: State -> WordleWord -> Response -> State
+advanceState state word response = state <> new_state
   where
+     -- a new State reflecting only information in the Response
+    new_state = MkState new_mapping
+
+    guess_with_response :: Vec5 (Char, LetterResponse)
+    guess_with_response = V.zip (getLetters word) (getResponses response)
+
+    new_mapping = V.ifoldl' go mempty guess_with_response
+      where
+        go :: M.Map Char LetterInformation -> Int -> (Char, LetterResponse) -> M.Map Char LetterInformation
+        go old_mapping _     (letter, Gray) = M.insert letter LetterNotInWord old_mapping
+        go old_mapping index (letter, Yellow) = M.insert letter (LetterNotInLocations (S.singleton index)) old_mapping
+        go old_mapping index (letter, Green) = M.insert letter (LetterNotInLocations (S.delete index (S.fromList allLocations))) old_mapping
+
+--------------------------------------
+-- filtering
+
+-- set of all letters that are required, according to the state
+requiredLetters :: State -> S.Set Char
+requiredLetters (MkState mapping) = M.foldlWithKey' go S.empty mapping
+  where
+    go :: S.Set Char -> Char -> LetterInformation -> S.Set Char
+    go already_required _current_letter LetterNotInWord = already_required
+    go already_required current_letter (LetterNotInLocations {}) = current_letter `S.insert` already_required
+
+validWord :: State -> WordleWord -> Bool
+validWord state@(MkState mapping) word = no_letters_in_bad_locations word && all_required_letters word
+  where
+    required_letters = requiredLetters state
+
+    -- check whether any letter is in a forbidden location
     no_letters_in_bad_locations :: WordleWord -> Bool
     no_letters_in_bad_locations word = V.ifoldl' go True (getLetters word)
       where
@@ -35,16 +135,16 @@ filterWords (MkState mapping) words = V.filter (\w -> no_letters_in_bad_location
           Just LetterNotInWord -> False
           Just (LetterNotInLocations excluded_locations) -> not (index `S.member` excluded_locations)
 
-    required_letters = M.foldlWithKey' go S.empty mapping
-      where
-        go :: S.Set Char -> Char -> LetterInformation -> S.Set Char
-        go already_required _current_letter LetterNotInWord = already_required
-        go already_required current_letter (LetterNotInLocations {}) = current_letter `S.insert` already_required
-
+    -- all the required letters are indeed in the word
     all_required_letters :: WordleWord -> Bool
-    all_required_letters word = required_letters `S.isSubsetOf` letters_in_word
-      where
-        letters_in_word = V.foldl' (flip S.insert) S.empty (getLetters word)
+    all_required_letters word = required_letters `S.isSubsetOf` lettersInWord word
+
+
+filterWords :: State -> V.Vector WordleWord -> V.Vector WordleWord
+filterWords state words = V.filter (validWord state) words
+
+------------------
+-- examples
 
 -- Suppose we get XXYXG for a guess of PEACH
 peachState :: State
